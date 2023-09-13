9762ff6780620ae11c46079cb5f6902c725ebab1
Karan <karan.trippybug@gmail.com>
2023-09-13 17:05:19 +0530

    ready for beta test

--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -105,7 +105,27 @@ const App: React.FC<{}> = () => {
   }, [getUrl]);
   
   
-  
+   ////////receiving message from popup.js////////
+   chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    if (request.message == true) {
+      chrome.storage.local.set({ isInstalled: true }, () => {
+        console.log("Value is set true");
+      });
+      window.location.reload();
+    } else if (request.message == false) {
+      chrome.storage.local.set({ isInstalled: false }, () => {
+        console.log("Value is set to false");
+      });
+      window.location.reload();
+    } else {
+      return;
+    }
+    sendResponse({ farewell: "Response from background script" });
+  });
 
   return <>
   <Tab/>
